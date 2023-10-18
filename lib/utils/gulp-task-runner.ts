9fd9a98ba3a7f4f6241d0eba06082b714525ca4e
Vova_Garvae <vgarvae@gmail.com>
2023-10-18 20:02:21 +0300

    [fix] legacy removed

--- lib/utils/gulp-task-runner.ts
+++
@@ -1,7 +0,0 @@
-import runSequence from 'gulp4-run-sequence';
-
-
-/**
- * Gulp tasks runner
- */
-export const gulpTaskRunner = async (task: unknown) => new Promise(resolve => runSequence(task, resolve));
